c2cd45d67836376cb2cc619bdbe565078f34e5a3
ronak <ronak04.exhibyte@gmail.com>
2021-09-17 18:18:33 +0530

    add file

---
+++ part -1/script.js
@@ -0,0 +1,186 @@
+
+/*let js = "wow!";
+if (js === "wow!") alert("javascript is fun!");
+
+ //10+20+90-60
+//  console.log(10+20+90-60)
+
+let fristname ="Exhibyte";
+console.log(fristname);
+console.log(fristname);
+
+
+let fun = true;
+console.log(fun);
+
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'Exhibyte');
+// console.log(typeof fun);
+
+javascript = 'YES!'
+console.log(typeof javascript)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null)
+
+
+// var and let and const
+let age = 30
+age = 31;
+console.log(age)
+
+const bdayyear = 2000;
+// bdayyear = 2001;
+// const job;
+
+var job  = "programmer"
+job = "teacher"
+console.log(job);
+
+
+
+// operator
+const now = 3000;
+const age = now - 2000;
+const secondage = now - 2007
+console.log(age, secondage);
+
+console.log(age * 2, secondage / 10, 2 ** 3);
+
+const fname = "Exhibyte";
+const lname = "soluation";
+console.log(fname + ' ' + lname);
+
+// assignment operator
+let x = 10 * 6 + 10;
+x += 10;
+x -= 20;
+x *= 2;
+x /= 3;
+console.log(x);
+
+// comparison operator
+console.log(age > secondage); // <,>,>=,<=
+console.log(age < secondage);
+console.log(age >= secondage);
+console.log(age <= secondage);
+
+
+// TASK-1
+/*Coding Challenge #1
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both 
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about 
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+m tall.
+GOOD LUCK */
+/*const massmark = 78;
+const hightmark = 1.69;
+const massjohn = 92;
+const hightjohn = 1.95;
+
+const BMImark = massmark / hightmark **2;
+const BMIjohn = massjohn / (hightjohn * hightjohn);
+const markhigherBMI = BMImark > BMIjohn;
+
+console.log(BMIjohn, BMImark, markhigherBMI);
+
+
+// STRING AND TEMPLATE LITERALS
+
+ const name = 'rahu';
+ const job = 'developer';
+ const bdayyear = 2000;
+ const year = 2099;
+
+ const jon = " i am " + name +', a'+ (year - bdayyear) + 'years old'+ job + '!';
+console.log(jon);
+
+const jonnew = `i am ${name}, a ${year - bdayyear} year old ${job}`;
+console.log(jonnew);
+
+console.log('start with \n\
+maultiple \n\
+line');
+
+
+// IF / Else condision
+const age = 18;
+const isoldEnough = age >= 19;
+
+if (isoldEnough) {
+    console.log("driving");
+}
+else {
+    console.log("not aligebale");
+}
+*/
+
+// Coding Challenge #2
+/*Use the BMI example from Challenge #1, and the code you already wrote, and 
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message 
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 
+GOOD LUCK */
+
+// const massmark = 78;
+// const hightmark = 1.69;
+// const massjohn = 92;
+// const hightjohn = 1.95;
+
+// const BMImark = massmark / hightmark ** 2;
+// const BMIjohn = massjohn / (hightjohn * hightjohn);
+
+// console.log(BMIjohn, BMImark);
+
+// if (BMImark > BMIjohn) {
+//     console.log("mark BMI is highe than john")
+// }
+// else {
+//     console.log("john BMI is highe than mark")
+// }
+
+
+
+// Type Conversion and Coercion
+
+/*const inputyear = '1991';
+console.log(Number(inputyear),inputyear);
+console.log(Number(inputyear) +19);
+
+console.log(Number('ronak'))
+console.log(typeof NaN);
+console.log(String(23), 23);
+
+// type coerction 
+console.log(' i am'+ 23 + 'year old');
+console.log(' i am'+ '23'+ 'year old');     
+console.log('23'-'10'-3);
+
+let n = '1' + 1;
+n = n -1;
+console.log(n);*/
+
+// Truthy and Falsy Values
+
