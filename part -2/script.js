2462d2619fb545c773a2e99b5c3327389d060f67
ronak <ronak04.exhibyte@gmail.com>
2021-09-18 16:52:48 +0530

    part-2 added

---
+++ part -2/script.js
@@ -0,0 +1,22 @@
+/*'use strict';
+let hasDriver = false;
+const passTest = true;
+
+if(passTest) hasDriver =true;
+if(hasDriver)console.log("i can drive");
+
+const private = 534;
+const interface = 'audio'
+*/
+
+function logger() {
+    console.log("my name ronak");
+}
+// calling / runing / invoking function
+logger();
+logger();
+
+function fruitprocessor(apples, orange) {
+ console.log(apples ,orange);
+ const juice = 
+}
