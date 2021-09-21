5c2729ea0538929a0a7afe9f17f980fd517cda83
ronak <ronak04.exhibyte@gmail.com>
2021-09-21 16:55:10 +0530

    part-2 completed

--- part -2/script.js
+++ part -2/script.js
@@ -413,15 +413,147 @@ if (mark.bmi > John.bmi) {
 
 /////////////////////////////////
 //LOOPING ARRAYS,BREAKING AND CONTINUING
-const jonas = [
+/*const jonas = [
   'Jonas',
   'Schmedtmann',
-  2037 - 1991,
+  2080,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+  // fillng types arry
+  // types[i] = typeof jonas [i];
+  types.push(typeof jonas[i])
+}
+console.log(types);
+
+const years = [1191, 2807, 1869, 2020];
+const age = [];
+
+for ( let i=0; i <years.length; i++){
+  // push method
+  age.push(2250 - years[i]);
+}
+console.log(age);
+
+// continue and break
+for (let i = 0; i<jonas.length; i++){
+  if(typeof jonas[i] !== 'string') continue;
+ 
+  console.log(jonas[i],typeof jonas[i]);
+}
+
+// for (let i = 0; i<jonas.length; i++){
+//   if(typeof jonas[i] !== 'Number') break;
+ 
+//   console.log(jonas[i],typeof jonas[i]);
+// }
+*/
+
+////////////////////////////
+//LOOPING BACKWARDS AND LOOPS IN LOOP
+
+/*const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2080,
   'teacher',
   ['Michael', 'Peter', 'Steven'],
   true
 ];
 
-for (let i =0; i<5; i++){
-  console.log(jonas[i]);
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
 }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`---starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`lifting weight repetation ${rep}`);
+  }
+}
+*/
+
+///////////////
+//while loop
+
+
+/*let rep =1;
+while(rep<=10){
+  console.log(`Exhibyte ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random()*6) + 1;
+console.log(dice);
+while(dice !== 6){
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random()*6) + 1 ;
+  if (dice === 6) console.log('loop is about to end...');
+}
+*/
+
+////////////////
+/*Coding Challenge #4
+
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate 
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the 
+tips and totals arrays 
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as 
+an argument. This function calculates the average of all numbers in the given 
+array. This is a difficult challenge (we haven't done this before)! Here is how to 
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition, 
+start by creating a variable 'sum' that starts at 0. Then loop over the 
+array using a for loop. In each iteration, add the current value to the 
+'sum' variable. This way, by the end of the loop, you have all values 
+added together
+4.2. To calculate the average, divide the sum you calculated before by the 
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+GOOD LUCK
+*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+
+
