ddb38d1f33a6ba28a73cbc7653f8219e47e8a861
ronak <ronak04.exhibyte@gmail.com>
2021-09-24 17:55:33 +0530

    data structures

---
+++ Data Structures, Modern Operators and Strings/script.js
@@ -0,0 +1,45 @@
+'use strict';
+/*
+const restaurant = {
+  name: 'classico italiano',
+  location: 'surat',
+  categories: ['otalian', 'pizza', 'pasta'],
+  startermenu: ['focaccia', 'bruschetta', 'garlic'],
+  mainmenu: ['browni', 'fryfrency'],
+  order: function (starterindex, mainindex) {
+    return [this.startermenu[starterindex], this.mainmenu[mainindex]];
+  },
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, x);
+console.log(arr);
+let [main, second] = restaurant.categories;
+console.log(main, second);
+
+// const temp = main;
+// main = second;
+// second = main;
+// console.log(main, second);
+[main, second] = [second, main];
+console.log(main, second);
+console.log(second, main);
+
+const [resto, ord] = restaurant.order(1, 0);
+console.log(resto, ord);
+
+const nested = [2, 4, [9, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+*/
+
+////////////
+//distructuring objects
